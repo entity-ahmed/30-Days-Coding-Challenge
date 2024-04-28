ff14ae37703185ef40472478959b3a8d45b9125f
AHMED <148766876+entity-ahmed@users.noreply.github.com>
2024-04-28 23:44:48 +0500

    Add files via upload

---
+++ Day 08/squares.py
@@ -0,0 +1,11 @@
+# Initialize an empty list to store the squares
+squares = []
+
+# Calculate and append squares of numbers from 1 to 5 to the list
+for i in range(1, 6):
+    squares.append(i ** 2)
+
+# Print the final list of squares
+print("List of squares of numbers from 1 to 5:")
+print(squares)
+
