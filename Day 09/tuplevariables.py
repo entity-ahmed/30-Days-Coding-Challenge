697ced94d70a6aead7431eb50f8ff3fe116578e3
AHMED <148766876+entity-ahmed@users.noreply.github.com>
2024-04-29 22:31:43 +0500

    Add files via upload

---
+++ Day 09/tuplevariables.py
@@ -0,0 +1,16 @@
+# Define two variables
+a = 10
+b = 20
+
+# Before swapping
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swap the values using tuple unpacking
+a, b = b, a
+
+# After swapping
+print("\nAfter swapping:")
+print("a =", a)
+print("b =", b)
